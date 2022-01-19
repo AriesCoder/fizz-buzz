58f3f9bef51a9a68bb27b1dcb767634626991429
Trang Lam <ms.tranglam@gmail.com>
2022-01-19 09:37:10 -0800

    solves the challenge

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,1 +1,13 @@
 // Write your solution below this line:
+for(let i = 1; i < 50; i++){
+    if(i % 3 ===0 && i % 5 ===0){
+        console.log(i, "fizzbuzz")
+    }else if(i % 3 === 0){
+        console.log(i, "fizz")
+    }else if(i % 5 === 0){
+        console.log(i, "buzz")
+    }else{
+        console.log(i)
+    }
+    
+}
